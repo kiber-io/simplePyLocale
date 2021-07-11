d7bbe47d9c2318ea8eeb58e3d9204aaaecb1537c
kiber.io <git@kiber.io>
2021-07-11 16:35:39 +0300

    rebase to module

--- simplePyLocale.py
+++ simplePyLocale.py
@@ -1,17 +1,21 @@
 import json
+from pathlib import Path
 
 language = 'en'
 translates = None
+langfile = None
 
 def set_language(lang):
     global language
-
     language = lang
+
+def set_language_file(file):
+    global langfile
+    langfile = Path(Path(__file__).parents[1].resolve(), file)
 
 def read_translates():
     global translates
-
-    with open('languages.json', 'r', encoding='utf-8') as f:
+    with open(langfile, 'r', encoding='utf-8') as f:
         try:
             translates = json.loads(f.read())
         except json.JSONDecodeError as e:
